a086998e8e976c1bcc11f45e8d70a3adf754fc98
Zhaozhe Wu <49044541+wzhaozhe@users.noreply.github.com>
2023-10-02 00:11:51 -0400

    Add files via upload

---
+++ Programming exercises/Ch2/Add3Integers.java
@@ -0,0 +1,17 @@
+/*
+ * File Add3Integers
+ * -----------------
+ * This program adds three integers and prints their sum
+ */
+import acm.program.*;
+
+public class Add3Integers extends ConsoleProgram{
+	public void run(){
+		println("This program adds three integers.");
+		int n1 = readInt("Enter n1:");
+		int n2 = readInt("Enter n2:");
+		int n3 = readInt("Enter n3:");
+		int total = n1 + n2 + n3;
+		println("Total is " + total);
+	}
+}
