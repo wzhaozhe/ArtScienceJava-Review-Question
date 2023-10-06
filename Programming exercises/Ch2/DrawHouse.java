4b82c174a0406eb268d8a2f26445214fb22405ec
Zhaozhe Wu <49044541+wzhaozhe@users.noreply.github.com>
2023-10-06 15:08:19 -0400

    Add files via upload

---
+++ Programming exercises/Ch2/DrawHouse.java
@@ -0,0 +1,21 @@
+/*
+ * File name: DrawHouse
+ * --------------------
+ * This program is draw a house with a peaked roof,
+ * two windows, and a door with a circular doorknob.
+ */
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class DrawHouse extends GraphicsProgram{
+	public void run(){
+		add(new GRect(200, 150, 300, 200));
+		add(new GLine(200, 150, 350, 50));
+		add(new GLine(350, 50, 500, 150));
+		add(new GRect(325, 200, 50, 150));
+		add(new GRect(225,175, 75, 75));
+		add(new GRect(400,175, 75, 75));
+		add(new GOval(360, 260, 10, 10));
+	}
+}
